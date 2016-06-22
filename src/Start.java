6ab1169bad4cee9d2fae1002d94b2e291c7a0c4b
milo101 <michael.lorenz@uni-duesseldorf.de>
2016-06-22 09:22:08 +0200

    Update Start.java
    Ich hab schonmal ein kleines Fenster erzeugt, damit wir schonmal was für de Start haben.

--- src/Start.java
+++ src/Start.java
@@ -1,6 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
-public class Start {
-	public static void main(String[] args){
-		System.out.println("Hello World");
+public class Start extends Application {
+
+    private Scene scene;
+
+    private Parent createContent(){
+        Pane root = new Pane();
+        root.setPrefSize(400, 400);
+	Label label = new Label("Das hier ist das Startprogramm");
+	label.setTranslateX(100);
+	label.setTranslateY(200);
+	root.getChildren().add(label);
+	return root;
 	}
+
+    @Override
+    public void start(Stage stage){
+        scene = new Scene(createContent());
+        stage.setTitle("Lieblingsprojekt");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void Start() {
+        launch();
+    }
 }
