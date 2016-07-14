265f4e4a33d7c0a7548fa7dabb571aaf168a117c
taroe100 <tanja.roeder@hhu.de>
2016-07-14 17:36:18 +0200

    Exit Maske: Sind Sie sich sicher, dass Sie beenden möchten?

--- Programm/src/main/java/ExitSpeichernActions.java
+++ Programm/src/main/java/ExitSpeichernActions.java
@@ -2,7 +2,12 @@ import java.util.Timer;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class ExitSpeichernActions extends LesenAusXml{
 	
@@ -13,15 +18,55 @@ public class ExitSpeichernActions extends LesenAusXml{
 	public void exitAction(Button exit, Timer timer)
 	{
 		exit.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
+			
 			public void handle(ActionEvent ae) {
-				timer.cancel();
-				System.exit(0);
+				
+				Stage stage_schliessen = new Stage();
+				stage_schliessen.setTitle("Schliessen");
+				try {
+					stage_schliessen.setScene(new Scene(schluss(stage_schliessen,timer)));
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				stage_schliessen.show();
 			}
 		});
-		
-		
 	}
+	
+	private Parent schluss(Stage stage_schliessen, Timer timer) throws Exception {
+
+					Pane root2 = new Pane();
+					root2.setPrefSize(250, 200);
+					
+					Label schliesstext = new Label("Möchten Sie wirklich beenden?\nBitte ans Abspeichern denken");
+					schliesstext.setLayoutX(50);
+					schliesstext.setLayoutY(20);
+					schliesstext.setId("schliesstext");
+					
+					Button close = new Button("Close");
+					
+					close.setLayoutX(50);
+					close.setLayoutY(100);
+					close.setId("close");
+					close.setStyle("-fx-text-fill: RED;");
+					close.setOnAction(new EventHandler<ActionEvent>() {
+						public void handle(ActionEvent ae) {
+							timer.cancel();
+							System.exit(0);
+					}
+					});
+					
+					root2.getChildren().add(schliesstext);
+					root2.getChildren().add(close);
+					return root2;
+					
+					}
+				
+		
+		
+		
+
 	
 	public void speichernAction(Button speichern, JavaFile klasseTest, JavaFile klasseMain)
 	{
@@ -34,4 +79,6 @@ public class ExitSpeichernActions extends LesenAusXml{
 		});
 	}
 
+
+	
 }
